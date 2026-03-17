767c362103925b9dd0913d35bb6d96b885b12e42
Giordano Lanna <giordanoal2003@gmail.com>
2026-03-17 00:56:26 -0300

    feature: - implementação de tela de login

--- mobile/src/app/login.tsx
+++ mobile/src/app/login.tsx
@@ -2,9 +2,42 @@ import React, { useState } from 'react'
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
 
 export default function Login() {
+  const [ userData, setUserData ] = useState<{ email: string, password: string}>({ email: '', password: ''})
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch('http://localhost:4000/login', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(userData)
+      })
+
+      if (response.ok) {
+        console.log('Usuário logado')
+        // Enviar toast de sucesso
+      }
+    } catch (error) {
+      // Enviar toast de erro
+    }
+  }
   return(
     <View>
-      <Text>Tela de Login</Text>
+      <Text>Bem-vindo!</Text>
+      <TextInput 
+        placeholder='E-mail'
+        value={userData.email} 
+        onChangeText={(email) => setUserData({...userData, email})}
+        />
+        
+      <TextInput
+        placeholder='password'
+        value={userData.password}
+        onChangeText={(password) => setUserData({...userData, password})}
+        secureTextEntry
+        />
+      <TouchableOpacity onPress={handleSubmit}>
+        <Text>Entrar</Text>
+      </TouchableOpacity>
     </View>
   )
 }
