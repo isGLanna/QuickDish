a1855595ae1a4e44cf4ba2f7958a61f23864ff2e
Giordano Lanna <giordanoal2003@gmail.com>
2026-03-18 23:56:55 -0300

    feat: implement delivery home screen with categories and search input

--- mobile/src/app/delivery-home.tsx
+++ mobile/src/app/delivery-home.tsx
@@ -1,9 +1,45 @@
-import { View, Text } from 'react-native'
+import { View, ScrollView, Text, TextInput, FlatList } from 'react-native'
+
+const categories = [
+  { id: 1, name: 'Pizza' },
+  { id: 2, name: 'Hambúrguer' },
+  { id: 3, name: 'Sushi' },
+  { id: 4, name: 'Caldo' },
+  { id: 5, name: 'Bebida' },
+  { id: 6, name: 'Açaí' },
+  { id: 7, name: 'Salada' },
+  { id: 8, name: 'Doce' },
+]
 
 export default function DeliveryHome() {
-  return(
-    <View>
-      <Text>Delivery Home</Text>
-    </View>
+  return (
+    <ScrollView>
+      {/* HEADER */}
+      <View>
+        <TextInput
+          placeholder='Qual seu pedido hoje?'
+          />
+      </View>
+
+      {/* CATEGORIES */}
+      <View/>
+        <FlatList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={categories}
+          keyExtractor={item => item.id}
+          renderItem={renderCategory}
+          />
+      </View>
+
+      {/* POPULAR ITEMS */}
+
+      <View>
+        <Text></Text>
+        <FlatList />
+      </View>
+
+  
+    </ScrollView>
   )
 }
