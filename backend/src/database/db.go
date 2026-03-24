0518b01aa561cc0f18c01030879acd6ec733157d
Giordano Lanna <giordanoal2003@gmail.com>
2026-03-24 15:32:57 -0300

    feat: implement database initialization and migration logic, add .env file support

--- backend/src/database/db.go
+++ backend/src/database/db.go
@@ -1,13 +1,59 @@
 package database
 
 import (
+	"backend/src/models"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
-type Database struct {
-	DB *gorm.DB
+var db *gorm.DB
+
+func initDB() {
+	if err := godotenv.Load(); err != nil {
+		log.Println(".env file not found")
+	}
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s db_name=%s port=%s sslmode=disable",
+		getEnv("DB_HOST", "localhost"),
+		getEnv("DB_USER", "postgres"),
+		getEnv("DB_PASSWORD", ""),
+		getEnv("DB_NAME", ""),
+		getEnv("DB_PORT", "5432"),
+	)
+
+	connection, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
+
+	if err != nil {
+		log.Fatal("Failed to connect with database", err)
+	}
+
+	if err := connection.AutoMigrate(
+		&models.Address{},
+		&models.Client{},
+		&models.Enterprise{},
+		&models.Food{},
+		&models.Order{},
+	); err != nil {
+		log.Fatal("Failed to migrate database", err)
+	}
+
+	db := connection
 }
 
-func (database *Database) Migrate(models ...interface{}) error {
-	return database.DB.AutoMigrate(models...)
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
 }
